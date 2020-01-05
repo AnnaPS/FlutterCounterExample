a720adfad2bf3239f692fb296ec420c43c360d74
Ana Polo Sánchez <ana.polo.sanchez@everis.com>
2020-01-05 17:56:50 +0100

    First commit

---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+// Class to run application
+import 'package:contador/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Center(
+          child: HomePage(),
+        ));
+  }
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,7 @@
+import 'package:contador/src/pages/counter_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => CounterPage();
+}
